ff40a7efff0dc8fe693c2f95a7a612759d8d3f75
rakzool <60981094+rakzool@users.noreply.github.com>
2021-06-05 01:29:31 +0530

    navbar UI fix

--- js/main.js
+++ js/main.js
@@ -18,3 +18,21 @@ right.addEventListener("mouseenter", () => {
 right.addEventListener("mouseleave", () => {
   container.classList.remove("hover-right");
 });
+
+const slidingIntoHerDms = () => {
+  const bars = document.querySelector(".bars");
+  const nav = document.querySelector(".nav-links");
+  const cross = document.querySelector(".cross");
+
+  bars.addEventListener("click", () => {
+    nav.classList.remove("nav-smooth");
+    nav.classList.add("nav-active");
+  });
+
+  cross.addEventListener("click", () => {
+    nav.classList.remove("nav-active");
+    nav.classList.toggle("nav-smooth");
+  });
+};
+
+slidingIntoHerDms();
