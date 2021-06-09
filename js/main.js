847d469c9d5107a11ec7ed8d678ddffbed428ae0
Ashutosh Kumar <raj.fungus@gmail.com>
2021-06-09 09:31:10 +0530

    preloder

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const container = document.querySelector(".container");
-
 left.addEventListener("mouseenter", () => {
   container.classList.add("hover-left");
 });
